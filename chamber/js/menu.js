066c085df0a93cc4c19b712f5110d66e3f15a696
Sopitive <30811278+Sopitive@users.noreply.github.com>
2022-01-28 23:44:46 -0700

    Home page for chamber.

---
+++ chamber/js/menu.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector("#hamburger");
+const nav = document.querySelector("#navigation");
+
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("responsive");
+})
+
