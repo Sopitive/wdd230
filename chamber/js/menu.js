a0bfecbf5368eee302bc32d0edac2d9ad9052994
Sopitive <30811278+Sopitive@users.noreply.github.com>
2022-02-03 16:54:38 -0700

    Update Layout

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector("#hamburger");
 const nav = document.querySelector("#navigation");
-
+const banner = document.querySelector(".current-date");
+const today = new Date()
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","Febuary","March","April","May","June","July","August", "September","October","November","December"]
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("responsive");
 })
+
+banner.appendChild(Object.assign(document.createElement("h3"), {
+    class: "banner-text",
+    innerHTML: `${weekday[today.getDay()]}, ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`
+}))
 
