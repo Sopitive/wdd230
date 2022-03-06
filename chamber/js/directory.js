1073c1ebf82d025c5987a468db178ca9797c66ec
Sopitive <30811278+Sopitive@users.noreply.github.com>
2022-03-05 22:50:10 -0700

    Added list view

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,6 +1,6 @@
 const data = "json/data.json"
 
-const main = document.querySelector("main");
+const main = document.querySelector(".cards");
 
 
 
@@ -20,9 +20,11 @@ function businessDisplay(business) {
 
     //The card that we will create that contains the information.
     const card = document.createElement("div");
+    card.classList.toggle("card-div")
 
     //Image
     const image = document.createElement("img");
+    image.classList.add('card-image');
     image.src = `images/${business.imgfile}`;
     image.alt = business.name;
     image.setAttribute("loading", "lazy");
@@ -51,3 +53,23 @@ function businessDisplay(business) {
 
     main.appendChild(card);
 }
+
+const button = document.querySelector(".toggle");
+const buttonImg = document.querySelector(".button-img");
+
+button.addEventListener("click", () => {
+    if (buttonImg.src.indexOf('cardlayout.svg') != -1) {
+        buttonImg.src = "images/list.svg";
+    } else {
+        buttonImg.src = "images/cardlayout.svg";
+    }
+    const cardImages = document.querySelectorAll(".card-image");
+    console.log(cardImages)
+    cardImages.forEach(function(item) {
+        item.classList.toggle('display');
+    });
+
+    const cards = document.querySelector(".cards");
+
+    cards.classList.toggle('cards-list');
+})
