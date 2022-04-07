aea4d0b1170e90793d8cd05ddab453b2a49026b7
Sopitive <30811278+Sopitive@users.noreply.github.com>
2022-04-06 20:19:24 -0600

    Return to previous page after form submission

--- TempleInnAndSuites/js/return.js
+++ TempleInnAndSuites/js/return.js
@@ -1,4 +1,12 @@
-//wait 5 seconds, then go back to the previous page
-setTimeout(function() {
-    window.history.back();
-}, 5000);
+const countdown = document.querySelector('.countdown');
+
+
+let timeleft = 6;
+let downloadTimer = setInterval(function() {
+    if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        window.history.back();
+    }
+    countdown.textContent = timeleft;
+    timeleft -= 1;
+}, 1000);
